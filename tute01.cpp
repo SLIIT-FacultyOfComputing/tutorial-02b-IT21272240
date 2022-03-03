9558f3215e02869a511fc16cc866d833fd35fcce
Rehan <100858781+IT21272240@users.noreply.github.com>
2022-03-03 15:45:26 +0000

    Tutorial 2 C++

--- tute01.cpp
+++ tute01.cpp
@@ -16,15 +16,18 @@ You already know that printf() in C is std::cout in C++ e.g.
 printf("Hello World") --> std::cout << "Hello World";
 2.54cm = 1 inch
 */
+#include <iostream>
+using namespace std;
 
-
-#include <stdio.h>
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+  
+    cout<<"Enter a length in cm : ";
+    cin>>cm;
+  
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+  
+    cout<<"Length in inches is "<<inches;
     return 0;
 }  
--- tute04.cpp
+++ tute04.cpp
@@ -10,20 +10,47 @@ Implement the functions
 long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
-
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
-long nCr(int n, int r);
+long nCr(int n, int r)
+{
+  int i,f=1;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+     
+    int o,k=1;
+    for(o=1;i<=r;o++)
+    {
+        k=k*o;
+    } 
+  
+    int h,l=1,a;
+     a=n-r;
+    for(h=1;i<a; h++)
+    {
+        l=l*h;
+    }
+    return f/k*l;
+}
 
-int main() {
-  int n, r;
+int main() 
+{
+  int n,r;
+  
   std::cout << "Enter a value for n ";
   std::cin >> n;
+  
   std::cout << "Enter a value for r ";
   std::cin >> r;
+  
   std::cout << "nCr = ";
+  
   std::cout << nCr(n,r);
+  
   std::cout << std::endl;
   return 0;
 }
